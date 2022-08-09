8c3f6339586271ff1d4cd051fa243b7ee1333ed7
Shivani-Dhoke <shivani.dhoke@eminds.ai>
2022-08-09 17:11:34 +0530

    changes in js

--- script/clock.js
+++ script/clock.js
@@ -8,8 +8,8 @@ setInterval(() => {
     mrotation = 6*mtime;
     srotation = 6*stime;
 
-    hour.style.transform = `(rotate{$hrotation}deg)`;
-    minute.style.transform = `(rotate{$mrotation}deg)`;
-    second.style.transform = `(rotate{$srotation}deg)`;
+    hour.style.transform = `rotate(${hrotation}deg)`;
+    minute.style.transform = `rotate(${mrotation}deg)`;
+    second.style.transform = `rotate(${srotation}deg)`;
 }, 1000);
   
