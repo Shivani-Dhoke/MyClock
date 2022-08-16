79f66e83d9d4d14ddd8e73513fd0c8c17f809554
Shivani-Dhoke <shivani.dhoke@eminds.ai>
2022-08-16 13:35:43 +0530

    changes in arrows

--- script/clock.js
+++ script/clock.js
@@ -8,8 +8,8 @@ setInterval(() => {
     mrotation = 6*mtime;
     srotation = 6*stime;
 
-    hour.style.transform = `rotate(${hrotation}deg)`;
-    minute.style.transform = `rotate(${mrotation}deg)`;
-    second.style.transform = `rotate(${srotation}deg)`;
+    document.getElementById('hour').style.transform = `rotate(${hrotation}deg)`;
+    document.getElementById('minute').style.transform = `rotate(${mrotation}deg)`;
+    document.getElementById('second').style.transform = `rotate(${srotation}deg)`;
 }, 1000);
   
